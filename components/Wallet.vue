12f96b4bbab2c24fe7b639bd5ccce1486d8111bd
ElFamosoComore <comore@como.re>
2021-05-07 17:20:17 +0200

    Creating Wallet component + adding wallet to the index page

---
+++ components/Wallet.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="box">
+        <img class="crypto_img" :src='logo' height="24">
+        <span><strong>{{ name }}</strong>: {{ wallet }}</span>
+    </div>
+</template>
+<script>
+export default {
+    props: [
+        'name',
+        'logo',
+        'wallet'
+    ]
+}
+</script>
+<style>
+
+.crypto_img {
+    margin-right: 5px;
+}
+
+.box {
+    display: flex;
+    flex-wrap: wrap;
+    text-align: center;
+    justify-content: center;
+}
+</style>
